bf18259bf01c6456e2cd35035b9bafec8d961d4b
Arslan-u-Hassan <arslanhassan1007@gmail.com>
2024-07-07 23:01:32 +0500

    Clean

--- app/workers/termsheet_email_send_worker.rb
+++ app/workers/termsheet_email_send_worker.rb
@@ -1,6 +1,5 @@
 class TermsheetEmailSendWorker
   include Sidekiq::Worker
-  # sidekiq_options queue: :email_send_queue
 
   def perform(house_loan_id, encoded_pdf)
     house_loan = HouseLoan.find(house_loan_id)
